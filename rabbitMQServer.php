8191da09f33fa4e474c7c2805b79c6787bf2f262
dylan <dylanschroers1@gmail.com>
2023-02-08 02:00:58 -0500

    authentication code

---
+++ rabbitMQServer.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('mysqlconnect.php');
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  //var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "Login":
+      return doLogin($request['username'],$request['password']);
+    case "validate_session":
+      return doValidate($request['sessionId']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+
+$server->process_requests('requestProcessor');
+exit();
+?>
+
