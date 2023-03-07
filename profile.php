f1bc1ec9a18244dfa33e5443fa5386be4e8c111b
John Zabriskie <98345161+JohnZabriskie@users.noreply.github.com>
2023-03-07 12:10:28 -0500

    needed to change the ip in testRabbitMQ.ini

---
+++ profile.php
@@ -0,0 +1,34 @@
+<?php
+
+//this is all for the profile and to grab info from DB
+
+
+$servername = "localhost";
+$username = "root";
+$password = "12345";
+$dbName = "Users";
+
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+
+//connection error
+if ($conn -> connect_error){
+	die("The connection failed: " . $conn->connect_error);}
+
+
+//query
+$query = "SELECT * FROM 'profile';";
+
+$result = $conn ->query($query);
+
+$row = $result->fetch_assoc("Username: " .
+			$row["username"]. " - First Name: " .
+			$row["fname"]. " - Last Name: " .
+			$row["lname"] . "<br>";
+	}
+}else{
+	echo "No Results";
+}
+
+$conn ->close();
+?>
