c726eb001773e4e8de661a574dd880c016755b09
EmmanuelChiobi <ec362@njit.edu>
2023-03-09 12:49:23 -0500

    Modifying login page

--- index.php
+++ index.php
@@ -27,37 +27,48 @@ function SendLoginRequest(formUname,formPword) {
 <style>
 	* {
 		background-color: lightblue;
+		padding: 2%;
 	}
-	.login_head {
-		padding: 90%;
+	#cent {
+		text-align: center;
+		padding: 1%;
 	}
-	#username, #password {
-		position: fixed;
+	#login {
+		padding: 1%;
+	}
+	.txtres {
+		text-align: center;
+	}
+	#register {
+		float: right;
 	}
 </style>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 <head>
 	<link rel="shortcut icon" href="#">
+	<!-- Header -->
+	<h1 id="cent">Login</h1>
+	<!-- Register -->
+	<button type="button" id="Register" class="btn btn-primary btn-block mb-4" value="Register">Register</button>
 </head>
-<h1 id="login_head">login page 2</h1>
 <body>
-<div id="textResponse">
-awaiting response2
-</div>
- <form id='loginForm'>
-  <!-- Username -->
-  <div class="form-outline mb-4">
-    <input type="username" id="form2Example1 username" class="form-control" placeholder="Username" />
-  </div>
+	<div id="login">
+		<form id='loginForm'>
+			<div class="txtres" id="textResponse">awaiting response2</div>
+			<!-- Username -->
+			<div class="form-outline mb-4">
+				<input type="username" id="form2Example1 username" class="form-control" placeholder="Username" />
+			</div>
 
-  <!-- Password -->
-  <div class="form-outline mb-4">
-    <input type="password" id="form2Example2 password" class="form-control" placeholder="Password" />
-  </div>
+			<!-- Password -->
+			<div class="form-outline mb-4">
+				<input type="password" id="form2Example2 password" class="form-control" placeholder="Password" />
+			</div>
 
-  <!-- Login Button -->
-  <button type="button" class="btn btn-primary btn-block mb-4" value="Login" onclick="SendLoginRequest(document.getElementById('username').value, document.getElementById('password').value);">Login</button>
-</form>
+			<!-- Login Button -->
+			<button type="button" class="btn btn-primary btn-block mb-4" value="Login" onclick="SendLoginRequest(document.getElementById('username').value, document.getElementById('password').value);">Login</button>
+		</form>
+	</div>
 </body>
 </html>
