d4d976029517b4b6cc7b39eace1c883a35fa13c1
John Zabriskie <98345161+JohnZabriskie@users.noreply.github.com>
2023-03-17 13:20:13 -0400

    adding profile code

--- profile.php
+++ profile.php
@@ -1,3 +1,6 @@
+	
+
+
 <?php
 
 //this is all for the profile and to grab info from DB
@@ -8,28 +11,57 @@ $username = "root";
 $password = "12345";
 $dbName = "Users";
 
-$conn = new mysqli($servername, $username, $password, $dbName);
+$conn = mysqli_connect($servername, $username, $password, $dbName);
 
 
 //connection error
-if ($conn -> connect_error){
-        die("The connection failed: " . $conn->connect_error);}
+if (!$conn){
+        die("The connection failed: " . mysqli_connect_error());}
 
 
 //query
-$query = "SELECT * FROM 'profile';";
+$query = "SELECT * FROM profile";
 
-$result = $conn ->query($query);
-
-$row = $result->fetch_assoc("Username: " .
-                        $row["username"]. " - First Name: " .
-                        $row["fname"]. " - Last Name: " .
-                        $row["lname"] . "<br>";
-        }
-}else{
-        echo "No Results";
+$result = mysqli_query($conn, $query);
+/*
+echo "<table>";
+echo"<tr><th>Username</th><th>First Name</th><th>Last Name</th></tr>";
+$row = mysqli_fetch_assoc($result){
+	echo "<tr><td>" .
+       		$row["username"] . "</td><td>" .
+	       	$row["fname"]. "</td><td>" .
+		$row["lname"] . "</td></tr>";
 }
+echo "</table>";
+}
+ */
 
-$conn ->close();
+
+mysqli_close($conn);
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Profile</title>
+</head>
+<body>
+	<h1>Profile</h1>
+	<table>
+	<tr><tr>
+		<th>Username</th>
+		<th>First Name</th>
+                <th>Last Name</th>
+		</tr>
+		   <?php $row = mysqli_fetch_assoc($result){?>
+		<tr>
+  		   <td><?php echo $row['username']; ?></td>
+		   <td><?php echo $row['fname']; ?></td>
+                   <td><?php echo $row['lname']; ?></td>
+		</tr> <?php} ?>
+	</table>
+</body>
+</html>
+
 
