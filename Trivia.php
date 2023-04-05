cf84ccb703237b3d32f824956a6e7a3c8c07273f
slevesque63 <sll24@njit.edu>
2023-04-05 10:16:43 -0400

    added website responsivity

--- Trivia.php
+++ Trivia.php
@@ -5,6 +5,7 @@ require('header.php');
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="triviastyle.css">
         <title>Trivia Game</title>
     </head>
--- recPage.php
+++ recPage.php
@@ -35,6 +35,9 @@
 		}
 	}
 </script>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 <body>
 	<form id ='search'>
 	<input type ='text' id ='showTS' placeholder ="Search..">
