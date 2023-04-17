94ae37dc230bfe11d217719d48115725c254dc1a
EmmanuelChiobi <ec362@njit.edu>
2023-04-16 21:49:06 -0400

    Edited nav-bar to have new group title

--- header.php
+++ header.php
@@ -20,7 +20,7 @@ require('functions.php');
 <body>
 <!-- Navigation Bar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="f">TVSite</a>
+  <a class="navbar-brand" href="f">ZLC-TV</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
